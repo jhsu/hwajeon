1a7ce0993a5fc45ad3660582930c69349853c0e4
helgi <helgith@gmail.com>
2013-07-19 16:51:43 +0100

    Fix up transform so names are top level

--- transform.rb
+++ transform.rb
@@ -8,19 +8,19 @@ all_resources = data["all_resources"]
 cookbooks = all_resources.map {|r| r["instance_vars"]["cookbook_name"]}.uniq
 
 data3 = {}
-data3["instance_vars"] = { "name" => data["node"]["name"] }
+data3['name'] = data["node"]["name"]
 data3["children"] = []
 
 cookbooks.each do |cookbook|
   cdata = {}
-  cdata["instance_vars"] = { "name" => cookbook}
+  cdata["name"] = cookbook
   cdata["children"] = []
   cresources = all_resources.select {|r| r["instance_vars"]["cookbook_name"] == cookbook}
   recipes = cresources.map {|r| r["instance_vars"]["recipe_name"]}.uniq
 
   recipes.each do |recipe|
     rdata = {}
-    rdata["instance_vars"] = { "name" => recipe }
+    rdata["name"] = recipe
     rdata["children"] = []
     rresources = cresources.select {|r| r["instance_vars"]["recipe_name"] == recipe}
     rresources.each do |resource|
