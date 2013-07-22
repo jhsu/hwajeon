0bc248949c77aeb63ae52ee4364bd3c56f83c346
helgi <helgith@gmail.com>
2013-07-22 01:11:40 +0100

    bring name and elapsed_time to a higher level (outside instance_vars) on every level and map size to elapsed_time for more compatibility data wise to other graph types

--- transform.rb
+++ transform.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'multi_json'
+require 'pp'
 
 json_file = 'chef.json'
 data = MultiJson.load(File.read(json_file))
@@ -8,22 +9,41 @@ all_resources = data["all_resources"]
 cookbooks = all_resources.map {|r| r["instance_vars"]["cookbook_name"]}.uniq
 
 data3 = {}
+data3['elapsed_time'] = data['elapsed_time']
+data3['size'] = data3['elapsed_time']
+data3['start_time'] = data['start_time']
+data3['end_time'] = data['end_time']
+data3["instance_vars"] = { "name" => data["node"]["name"] }
 data3['name'] = data["node"]["name"]
 data3["children"] = []
 
 cookbooks.each do |cookbook|
-  cdata = {}
-  cdata["name"] = cookbook
-  cdata["children"] = []
   cresources = all_resources.select {|r| r["instance_vars"]["cookbook_name"] == cookbook}
+
+  cdata = {}
+  cdata["instance_vars"] = { "name" => cookbook}
+  cdata['name'] = cookbook
+  cdata['elapsed_time'] = cresources.first['instance_vars']['elapsed_time']
+  cdata['size'] = cdata['elapsed_time']
+  cdata["children"] = []
+  pp cookbook
+  pp cdata['size']
   recipes = cresources.map {|r| r["instance_vars"]["recipe_name"]}.uniq
 
   recipes.each do |recipe|
-    rdata = {}
-    rdata["name"] = recipe
-    rdata["children"] = []
     rresources = cresources.select {|r| r["instance_vars"]["recipe_name"] == recipe}
+
+    rdata = {}
+    rdata["instance_vars"] = { "name" => recipe }
+    rdata['name'] = recipe
+    rdata['elapsed_time'] = rresources.first['instance_vars']['elapsed_time']
+    rdata['size'] = rdata['elapsed_time']
+    rdata["children"] = []
+    pp rdata['size']
     rresources.each do |resource|
+      resource['name'] = resource['instance_vars']['name']
+      resource['elapsed_time'] = resource['instance_vars']['elapsed_time']
+      resource['size'] = resource['elapsed_time']
       rdata["children"] << resource
     end
     cdata["children"] << rdata
